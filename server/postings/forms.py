5216b0d2ab3b44c8de7af43a3af57a52639f3e16
Abdullaev991 <oskarabdullaev07@gmail.com>
2024-07-04 00:21:14 +0300

    Feature/upd user profile (#1)
    * test-commit
    
    * add profile_update_page
    fix theme switcher
    fix redirects
    add profile_page_others

--- server/postings/forms.py
+++ server/postings/forms.py
@@ -1,8 +1,23 @@
 from datetime import datetime
 
-from django import forms
 from PIL import Image
+from django import forms
+from django.contrib.auth.forms import UserChangeForm
+from django.contrib.auth.models import User
 from postings.models import PostModel, CommentModel
+
+
+
+
+class ProfileUpdateForm(UserChangeForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control text-bg-light'}))
+    first_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control text-bg-light'}))
+    last_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control text-bg-light'}))
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control text-bg-light'}))
+
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username', 'email']
 
 
 class LoginForm(forms.Form):
@@ -14,15 +29,15 @@ class LoginForm(forms.Form):
 
         self.fields['username'].widget.attrs['placeholder'] = 'Введите ваше имя'
         self.fields['password'].widget.attrs['placeholder'] = 'Введите ваш пароль'
-        self.fields['username'].widget.attrs['class'] = 'form-control'
-        self.fields['password'].widget.attrs['class'] = 'form-control'
+        self.fields['username'].widget.attrs['class'] = 'form-control text-bg-light'
+        self.fields['password'].widget.attrs['class'] = 'form-control text-bg-light'
         self.fields['username'].widget.attrs['id'] = 'usernameInput'
         self.fields['password'].widget.attrs['id'] = 'passwordInput'
 
 
 class PostForm(forms.Form):
     title = forms.CharField(label='Заголовок', required=True)
-    desc = forms.CharField(label='Описание', required=True,widget=forms.Textarea)
+    desc = forms.CharField(label='Описание', required=True, widget=forms.Textarea)
     file = forms.ImageField(required=False)
 
     def __init__(self, *args, **kwargs):
@@ -32,45 +47,48 @@ class PostForm(forms.Form):
         self.fields['desc'].widget.attrs['placeholder'] = 'Введите описание поста'
         self.fields['title'].widget.attrs['id'] = 'titleInput'
         self.fields['desc'].widget.attrs['id'] = 'descInput'
-        self.fields['title'].widget.attrs['class'] = 'form-control'
-        self.fields['desc'].widget.attrs['class'] = 'form-control'
-        self.fields['file'].widget.attrs['class'] = 'form-control'
+        self.fields['title'].widget.attrs['class'] = 'form-control text-bg-light'
+        self.fields['desc'].widget.attrs['class'] = 'form-control text-bg-light'
+        self.fields['file'].widget.attrs['class'] = 'form-control text-bg-light'
         self.fields['file'].widget.attrs['id'] = 'fileInput'
 
     def save(self, user):
         if self.is_valid():
-            image = Image.open(self.cleaned_data.get("file"))
+            if self.cleaned_data['file'] is not None:
+                image = Image.open(self.cleaned_data.get("file"))
 
-            last_model_object = PostModel.objects.last()
-            if last_model_object is None:
-                file_number = 1
+                last_model_object = PostModel.objects.last()
+                if last_model_object is None:
+                    file_number = 1
+                else:
+                    file_number = last_model_object.id + 1
+
+                file_name = f"{file_number}.{image.format}"
+                image.save(f"static/images/{file_name}")
+                image.close()
             else:
-                file_number = last_model_object.id + 1
-
-            file_name = f"{file_number}.{image.format}"
-            image.save(f"static/images/{file_name}")
-            image.close()
-
+                file_name = None
             PostModel(
                 author=user,
                 title=self.cleaned_data.get("title"),
                 desc=self.cleaned_data.get("desc"),
                 file_name=file_name,
-                created = datetime.now(),
-                update_author = user,
+                created=datetime.now(),
+                update_author=user,
 
-                ).save()
+            ).save()
 
 
 class CommentForm(forms.Form):
-    text = forms.CharField(label='Текст комментария')
+    text = forms.CharField(label='')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.fields['text'].widget.attrs['class'] = 'form-control'
+        self.fields['text'].widget.attrs['class'] = 'form-control text-bg-light'
+        self.fields['text'].widget.attrs['placeholder'] = 'Написать комментарий...'
         self.fields['text'].widget.attrs['id'] = 'textInput'
-
+        self.fields['text'].widget.attrs['name'] = 'ftext'
 
     def save(self, photo, user):
         if self.is_valid():
@@ -78,5 +96,5 @@ class CommentForm(forms.Form):
                 photo=photo,
                 text=self.cleaned_data.get("text"),
                 author=user,
-                created = datetime.now()
+                created=datetime.now()
             ).save()
--- server/postings/models.py
+++ server/postings/models.py
@@ -1,18 +1,15 @@
 from django.db import models
-from django.contrib.auth.models import User, AbstractUser
-from django.contrib.auth import get_user_model
-from django.core.validators import FileExtensionValidator
-from django.urls import reverse
-from datetime import date, timedelta
+
 
 class PostModel(models.Model):
     author = models.ForeignKey("auth.User", on_delete=models.CASCADE, related_name="author")
     title = models.TextField()
     desc = models.TextField()
-    file_name = models.TextField()
+    file_name = models.TextField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     udpated = models.DateTimeField(auto_now=True)
     update_author = models.ForeignKey("auth.User", on_delete=models.CASCADE, related_name="update_author")
+
 
 class CommentModel(models.Model):
     author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
